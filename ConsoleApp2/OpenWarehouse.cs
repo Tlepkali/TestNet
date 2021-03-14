7a9638ff1f7be5af4599b13a1e76692838a0165b
Tlepkali Yerkingaliyev <erkingalievt@gmail.com>
2021-03-15 04:09:14 +0600

    preTask3

---
+++ ConsoleApp2/OpenWarehouse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class OpenWarehouse
+    {
+        public double area;
+        public Dictionary <Item, int> items = new Dictionary<Item, int>();
+
+        public Address address;
+        public ResponsibleEmployee responsibleEmployee;
+    }
+}
